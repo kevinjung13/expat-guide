cddb974e268c46064029471e0cdf8ef41a2ba4a3
Kevin Jung <kevinjung13@gmail.com>
2023-04-28 16:14:07 +0700

    Started structuring explore and neighbourhood sections

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -21,212 +21,38 @@ export default function Explore() {
         </section>
       </SectionLayout>
       {/* Second Section */}
-      <section className={"bg-gray-200 min-h-screen pt-16"}>
-        <div className={"flex place-content-center"}>
-          <Link href={"/explore/food"}>
-            <h1 className={"text-5xl hover:text-blue-800"}>DINING</h1>
-          </Link>
-        </div>
-        <div className={"flex-col place-content-center p-6"}>
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-          <br />
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-        </div>
-      </section>
-      {/* Third Section */}
-      <section className={"bg-green-500 min-h-screen pt-16"}>
-        <div className={"flex place-content-center"}>
-          <h1 className={"text-5xl "}>ACTIVITIES</h1>
-        </div>
-        <div className={"flex-col place-content-center p-6"}>
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-          <br />
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-        </div>
-      </section>
-      {/* Fourth Section */}
-      <section className={"bg-red-500 min-h-screen pt-16"}>
-        <div className={"flex place-content-center"}>
-          <h1 className={"text-5xl "}>ATTRACTIONS</h1>
-        </div>
-        <div className={"flex-col place-content-center p-6"}>
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-          <br />
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-        </div>
-      </section>
-      {/* Fifth Section */}
-      <section className={"bg-yellow-500 min-h-screen pt-16"}>
-        <div className={"flex place-content-center"}>
-          <h1 className={"text-5xl "}>NIGHTLIFE</h1>
-        </div>
-        <div className={"flex-col place-content-center p-6"}>
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-          <br />
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-        </div>
-      </section>
-      {/* Sixth Section */}
-      <section className={"bg-teal-500 min-h-screen pt-16"}>
-        <div className={"flex place-content-center"}>
-          <h1 className={"text-5xl "}>Nearby Destinations</h1>
-        </div>
-        <div className={"flex-col place-content-center p-6"}>
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-          <br />
-          <p className={"text-lg "}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            eget aliquam nunc. Sed eget nisl mattis, vestibulum neque porttitor,
-            vehicula sapien. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus pharetra turpis sit
-            amet ante pharetra, nec feugiat dui commodo. Vivamus sed hendrerit
-            lacus, vitae ullamcorper sem. Suspendisse nec arcu libero. Proin mi
-            ante, varius quis commodo quis, pharetra eget metus. Sed in nibh ut
-            arcu ultricies varius eu ac ligula. Nunc bibendum, odio vel lobortis
-            porttitor, ligula mi bibendum elit, sed aliquam dui risus a odio.
-            Etiam tempus, est vitae laoreet lobortis, justo nibh tempor ligula,
-            a tristique velit velit facilisis quam. Vivamus tincidunt enim a
-            libero luctus, et dapibus tellus rhoncus. Nunc vel ullamcorper mi.
-            Praesent maximus dictum est, et mollis turpis dignissim eget. Donec
-            commodo odio in metus posuere lobortis.
-          </p>
-        </div>
-      </section>
+      <section>
+            <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 md:px-8">
+                <div className="space-y-5 max-w-4xl mx-auto text-center">
+                    <h2 className="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
+                        The city is <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]">your oyster</span>
+                    </h2>
+                    <p className="max-w-2xl mx-auto">
+                        Sed ut perspiciatis unde omnis iste natus voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
+                    </p>
+                    <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
+                        <Link href="javascript:void(0)" className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
+                            Dining
+                        </Link>
+                        <Link href="javascript:void(0)" className="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                            Activities
+              </Link>
+              <Link href="javascript:void(0)" className="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                            Attractions
+              </Link>
+              <Link href="javascript:void(0)" className="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                            Nightlife
+              </Link>
+              <Link href="javascript:void(0)" className="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                            Nearby Destinations
+                        </Link>
+                    </div>
+                </div>
+                <div className="mt-14">
+                    <img src="/img/explore/dine-img.jpg" className="w-full shadow-lg rounded-lg border" alt="" />
+                </div>
+            </div>
+        </section>
     </>
   );
 }
